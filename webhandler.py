506047d2b4cb49218b4af04e1e2e15085143c3a5
g0tmi1k <have.you.g0tmi1k@gmail.com>
2012-07-06 17:16:42 +0100

    Checks to see if the url starts with "http" and adds it if its missing

--- webhandler.py
+++ webhandler.py
@@ -20,10 +20,10 @@ from core.modules.menu import getargs, get_banner
 # check for arguments dependencies
 if getargs.url:
     if getargs.method == 'post' and not getargs.parameter:
-        print '\n[!] Using post method requires --parameter flag, check examples'
+        print '\n[!] Using "post" method requires --parameter flag, check examples on the help screen'
         exit(1)
     if getargs.method == 'get' and getargs.parameter:
-        print '\n[!] Using get method doesn\'t require --parameter flag, check examples'
+        print '\n[!] Using "get" method doesn\'t require --parameter flag, check examples on the help screen'
         exit(1)
     else:
         print get_banner                            # get WebHandler banner
