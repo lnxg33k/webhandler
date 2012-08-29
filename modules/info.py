5fccff2b5505902f8c8fa1498b2446bde75c40d3
lnxg33k <ahmed@isecur1ty.org>
2012-08-29 21:17:51 +0200

    Major code refactoring

--- modules/info.py
+++ modules/info.py
@@ -9,10 +9,10 @@ from core.libs.thirdparty.termcolor import colored
 class TargetBox(object):
     def __init__(self, url=None):
         self.url = url
-        self.cmd = 'whoami;'
-        self.cmd += 'id;'
-        self.cmd += 'uname -a;'
-        self.cmd += 'pwd;'
+        self.cmd = 'if [ `whoami 2> /dev/null|wc -l` -ge 1 ];then whoami;else echo "unkown" ;fi;'
+        self.cmd += 'if [ `id 2> /dev/null|wc -l` -ge 1 ];then id;else echo "unknown" ;fi;'
+        self.cmd += 'if [ `uname 2> /dev/null|wc -l` -ge 1 ];then uname -a;else echo "unknown" ;fi;'
+        self.cmd += 'if [ `pwd 2> /dev/null|wc -l` -ge 1 ];then pwd;else echo "unknown" ;fi;'
         self.cmd += 'ls -ld `pwd` | awk \'{print $1}\';'
         self.cmd += 'bash -c "input=\$(uptime); if [[ \$input == *day* ]]; then out=\$(echo \$input | awk \'{print \$3\\" days\\"}\'); if [[ \$input == *min* ]]; then out=\$(echo \\"\$out and \\" && echo \$input | awk \'{print \$5\\" minutes\\"}\'); else out=\$(echo \\"\$out, \\" && echo \$input | awk \'{print \$5}\' | tr -d \\",\\" | awk -F \\":\\" \'{print \$1\\" hours and \\"\$2\\" minutes\\"}\'); fi elif [[ \$input == *min* ]]; then out=\$(echo \$input | awk \'{print \$3\\" minutes\\"}\'); else out=\$(echo \$input | awk \'{print \$3}\' | tr -d \\",\\" | awk -F \\":\\" \'{print \$1\\" hours and \\"\$2\\" minutes\\"}\'); fi; echo \$out;" ;'
         self.cmd += "/sbin/ifconfig | grep -e 'inet addr' | grep -v '127.0.0.1' | cut -f2 -d':' | cut -f1 -d' ';"
@@ -25,19 +25,14 @@ class TargetBox(object):
         # Call get_page_source() method then assign it to self.source
         source = make_request.get_page_source(self.cmd)
 
-        def get(seq, index, default='Unknown'):
-            try:
-                return seq[index]
-            except:
-                return default
-
-        self.current_user = get(source, 0)
-        self.current_id = get(source, 1)
-        self.kernel_info = get(source, 2)
-        self.cwd = get(source, 3)
-        self.perm_cwd = get(source, 4)
-        self.uptime = get(source, 5)
-        self.host_ip = get(source, 6)
+        source = iter(source)
+        self.current_user = next(source, "Unknown")
+        self.current_id = next(source, "Unknown")
+        self.kernel_info = next(source, "Unknown")
+        self.cwd = next(source, "Unknown")
+        self.perm_cwd = next(source, "Unknown")
+        self.uptime = next(source, "Unknown")
+        self.host_ip = next(source, "Unknow")
         self.session = now.strftime("%Y-%m-%d")
         self.url = '/'.join(getargs.url.split('/', 3)[:3])
         try:
